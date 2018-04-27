d63b955fdbbb42f44034776e2aa97c87f0e6784a
len007 <31910046+len007@users.noreply.github.com>
2018-04-27 11:47:53 +0800

    update-0427

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
-  providers: [
-    HomeComponent
-  ]
+  providers: [ ]
 })
-export class AppComponent  { name = '郑小丽'; }
+export class AppComponent  { name = 'HZ'; }
