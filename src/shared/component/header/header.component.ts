25233cb2cf1614ab9ac6f2d85089f664857a011c
len007 <31910046+len007@users.noreply.github.com>
2018-04-27 11:45:16 +0800

    update-0427

---
+++ src/shared/component/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'header-content',
+    moduleId: module.id,
+    template: `<h1>Hello，这是标题</h1>`
+})
+export class HeaderComponent { }
