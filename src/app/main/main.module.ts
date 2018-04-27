8582e9ca5e4230db84e94d5752710b0832b511f9
len007 <31910046+len007@users.noreply.github.com>
2018-04-27 11:51:21 +0800

    update-0427

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,23 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { MainRouterModule } from './main-router.module'
 import { MainComponent } from './main.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { ListModule } from '../list/list.module';
 import { HomeModule } from '../home/home.module';
-import { HomeComponent } from '../home/home.component';
-import { ListComponent } from '../list/list.component';
-
 
 @NgModule({
     imports: [
         BrowserModule,
+        ListModule,
+        HomeModule,
         RouterModule.forChild(MainRouterModule)
     ],
     declarations: [
         MainComponent,
-        HomeComponent,
-        ListComponent
     ],
     exports: [
         MainComponent
