0af008a776f4c5e00c23c5dece197aa4212d5203
len007 <31910046+len007@users.noreply.github.com>
2018-04-27 11:43:40 +0800

    update-0427

---
+++ src/shared/component/left-menu/leftmenu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'left-menu-content',
+    moduleId: module.id,
+    template: `<h1>Hello，这是左边菜单栏</h1>`
+})
+export class LeftMenuComponent { }
