8954099e8a3904a66b075cf0a89bc4c862f6088e
len007 <31910046+len007@users.noreply.github.com>
2018-04-27 12:11:27 +0800

    update-0427

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 import { AppComponent } from './app.component';
 import { AppRouterModule } from './app-router.module';
 import { MainModule } from './main/main.module';
 import { HeaderModule } from '../shared/component/header/header.module';
 import { LeftMenuModule } from '../shared/component/left-menu/leftmenu.module';
+// import { AvenueUICoreModule } from '@avenueui/core';
 
 @NgModule({
   imports: [
@@ -13,6 +15,7 @@ import { LeftMenuModule } from '../shared/component/left-menu/leftmenu.module';
     MainModule,
     HeaderModule,
     LeftMenuModule,
+    SharedModule.forRoot(),
     RouterModule.forRoot(AppRouterModule)
   ],
   declarations: [
