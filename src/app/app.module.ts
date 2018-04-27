1f3d5cf25c8c5ba84a9e014a7b821079d17e627e
len007 <31910046+len007@users.noreply.github.com>
2018-04-27 11:48:32 +0800

    update-0427

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { AppRouterModule } from './app-router.module'
+import { AppRouterModule } from './app-router.module';
 import { MainModule } from './main/main.module';
-
+import { HeaderModule } from '../shared/component/header/header.module';
+import { LeftMenuModule } from '../shared/component/left-menu/leftmenu.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     MainModule,
+    HeaderModule,
+    LeftMenuModule,
     RouterModule.forRoot(AppRouterModule)
   ],
   declarations: [
